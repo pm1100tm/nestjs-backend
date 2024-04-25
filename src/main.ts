3d889d7fbdd9978fc2eec4e56bd4db5fbbed2204
swd <tladnjsen@gmail.com>
2024-04-25 22:58:19 +0900

    chore: Enable class validator with global scope

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,16 @@
 import { NestFactory } from '@nestjs/core';
+import { ValidationPipe } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { abortOnError: false });
-  await app.listen(3000);
+
+  app.useGlobalPipes(new ValidationPipe({ transform: true }));
+  const configService = app.get(ConfigService);
+  const port = Number(configService.get('PORT'));
+
+  await app.listen(port);
 }
 bootstrap();
