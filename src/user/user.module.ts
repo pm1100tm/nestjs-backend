5831052b833ac7ed6b657039df654c1b1fbfef4d
swd <tladnjsen@gmail.com>
2024-04-25 23:23:52 +0900

    modify: Modify user module

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,11 +1,19 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
 import { UserController } from './controllers/user.controller';
 import { CustomerController } from './controllers/customer.controller';
 import { UserService } from './services/user.service';
 import { CustomerService } from './services/customer.service';
 
+import { User } from './entities/user.entity';
+import { Customer } from './entities/customer.entity';
+import { UserRepository } from './repositories/user.repository';
+import { CustomerRepository } from './repositories/customer.repository';
+
 @Module({
+  imports: [TypeOrmModule.forFeature([User, Customer])],
   controllers: [UserController, CustomerController],
-  providers: [UserService, CustomerService],
+  providers: [UserService, CustomerService, UserRepository, CustomerRepository],
 })
 export class UserModule {}
