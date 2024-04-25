4a465cb01d1c8fb42565da8d343782e30286bd11
swd <tladnjsen@gmail.com>
2024-04-25 23:02:19 +0900

    Trivial

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,7 +1,11 @@
 import { Module } from '@nestjs/common';
 import { UserController } from './controllers/user.controller';
+import { CustomerController } from './controllers/customer.controller';
+import { UserService } from './services/user.service';
+import { CustomerService } from './services/customer.service';
 
 @Module({
-  controllers: [UserController],
+  controllers: [UserController, CustomerController],
+  providers: [UserService, CustomerService],
 })
 export class UserModule {}
