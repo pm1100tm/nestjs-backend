b6c6da6e6114a139c7b5cc796a5accbc114c01cd
swd <tladnjsen@gmail.com>
2024-04-26 18:46:22 +0900

    feat: Add guard
    - accessTokenGuard
    - refreshTokenGuard

---
+++ src/auth/guards/refreshToken.gurad.ts
@@ -0,0 +1,57 @@
+import { Request as Req } from 'express';
+import {
+  Injectable,
+  CanActivate,
+  ExecutionContext,
+  UnauthorizedException,
+} from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { JwtService } from '@nestjs/jwt';
+import { JwtPayload, Tokens } from 'auth/auth.types';
+
+@Injectable()
+export class RefreshTokenGuard implements CanActivate {
+  constructor(
+    private readonly configService: ConfigService,
+    private readonly jwtService: JwtService,
+  ) {}
+
+  async canActivate(context: ExecutionContext): Promise<boolean> {
+    const request = context.switchToHttp().getRequest();
+    const { refreshToken } = this.extractTokenFromHeader(request);
+    if (!refreshToken) {
+      throw new UnauthorizedException();
+    }
+
+    try {
+      const payload: JwtPayload = await this.jwtService.verifyAsync(
+        refreshToken,
+        {
+          secret: this.configService.get<string>('JWT_REFRESH_SECRET'),
+        },
+      );
+
+      request.user = {
+        ...payload,
+        refreshToken,
+      };
+    } catch (e) {
+      throw new UnauthorizedException();
+    }
+
+    return true;
+  }
+
+  private extractTokenFromHeader(request: Req): { refreshToken: string } {
+    const cookies = request.headers?.cookie || '';
+    if (!cookies) {
+      throw new UnauthorizedException('Not exists cookie string');
+    }
+
+    const encodedJson = cookies.split('auth-cookie=')[1];
+    const decodedJson = decodeURIComponent(encodedJson);
+    const token: Tokens = JSON.parse(decodedJson);
+
+    return { refreshToken: token.refreshToken };
+  }
+}
