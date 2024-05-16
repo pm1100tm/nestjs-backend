4e649acdd4fc7e6dbb6bb98c90c636050253efc3
swd <tladnjsen@gmail.com>
2024-05-16 16:25:00 +0900

    Trivial

--- docker/init-local.sql
+++ docker/init-local.sql
@@ -1,15 +1,18 @@
+-- Use this script to create tables or data seed it if needed.
+-- For now, it's not being used but use scripts under the scripts in migrations folder.
+
 -- -- Create user and grant privileges
 -- CREATE USER root
 --   WITH PASSWORD '1234'
 -- ;
 
 -- -- Set permission
--- GRANT ALL PRIVILEGES ON DATABASE cully TO root;
+-- GRANT ALL PRIVILEGES ON DATABASE backend TO postgres;
 
 -- -- Create database
 -- CREATE DATABASE <db_name>
 --   WITH
---     OWNER = root
+--     OWNER = postgres
 --     ENCODING = 'UTF8'
 --     LC_COLLATE = 'en_US.utf8'
 --     LC_CTYPE = 'en_US.utf8'
